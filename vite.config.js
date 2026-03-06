d76c5c6c559820180b22823d0dea92dd1751cb6e
Женя <mishagolub301@gmail.com>
2026-03-07 00:33:07 +0200

    added vite config external + fixed build

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // если на подпути — измени
+  build: {
+    rollupOptions: {
+      external: ['@walletconnect/ethereum-provider', '@walletconnect/sign-client', '@walletconnect/universal-provider']
+    }
+  }
 })
